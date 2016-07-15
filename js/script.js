7537af59e668e1bf5941db5d50b902d5ddd7dc7d
Ростислав <extra0@mail.ru>
2016-07-15 17:47:14 +0300

    этап разработки header

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
 $(function(){
 
-	// вызов фенсибокса
-	$('.js-fancybox').fancybox();
-
 	// разделяем числа на разряды
-	function numberWithCommas(x) {
-		return x.toString().replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g, "\$1 ");
-	}
-	$('.js-replace-number').each(function() {
-		$(this).html(numberWithCommas($(this).html()));
-	});
+	function numberWithCommas(x) { return x.toString().replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g, "\$1,");}
+	$('.js-replace-number').each(function() {$(this).html(numberWithCommas($(this).html()));});
 
 	// collapse block
 	$('.js-slide-trigger').on('click', function(){
